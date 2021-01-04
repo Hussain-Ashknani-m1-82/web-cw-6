b17aa600cf4346a54166d3e68cb6894ca1f21375
Hussain-Ashknani-m1-82 <76279346+Hussain-Ashknani-m1-82@users.noreply.github.com>
2021-01-04 15:15:40 +0300

    تم الحل

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,12 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+
+let uname = prompt(`What is Your Name ??`);
+
+if(uname==(``)){
+    alert(`ERROR please Type The Name!!!`)
+
+}else{
+    alert(`Hello ${uname} Welcome :) `)
+}
+
+ 
